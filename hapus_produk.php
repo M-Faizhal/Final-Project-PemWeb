0730ba07fb56bd51d8a4cf382a5ca16fcc8e4195
M-Faizhal <faizhalmusthafa@gmail.com>
2024-05-22 12:33:58 +0700

    hapus

--- hapus_produk.php
+++
@@ -1,9 +0,0 @@
-<link rel="stylesheet" href="admin/assets/css/bootstrap.css">
-<?php
-session_start();
-$id_produk = $_GET["id"];
-unset($_SESSION["keranjang"][$id_produk]);
-
-echo "<script>alert('Produk Di Hapus');</script>";
-echo "<script>location='keranjang.php';</script>";
-?>
--- logout.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-
-session_destroy();
-echo "<script>alert('Anda Logout');</script>";
-echo "<script>location='index.php'</script>";
-?>
